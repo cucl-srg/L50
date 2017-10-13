7d90dfec106db3ffe75e86215f96e5c13037150f
Noa Zilberman <nz247@cl.cam.ac.uk>
2017-10-13 14:40:00 +0100

    fix to scripts

--- Jupyter/useful/useful1.py
+++ Jupyter/useful/useful1.py
@@ -1,4 +1,6 @@
 from useful import np,plt,findall,ceil,floor
+import os
+
 
 def ind_of(value,arr):
     for j in range(len(arr)):
@@ -19,10 +21,10 @@ def largest_index(value, arrarr):
         larges.append(ind_of(value,arrarr[i]))
     return max(larges)
     
-def getrtts(exp,num):
+def getrtts(exp,crsid,num):
     rtts= [[] for i in range(10)]
     for i in range(10):
-        with open('/root/L50Lab1/'+exp+'_'+str(i)) as f:
+        with open('/root/'+crsid+'/L50Lab1/'+exp+'_'+str(i)) as f:
             for j in range(num):
                 rt = f.next()
                 rt = findall(r'time=(.*?) ms',rt)[0]
@@ -32,9 +34,9 @@ def getrtts(exp,num):
 
 # you will use functions under this line
 
-def getrtt(ffile,num):
+def getrtt(ffile,crsid,num):
     rtt= []
-    with open('/root/L50Lab1/'+ffile) as f:
+    with open('/root/'+crsid+'/L50Lab1/'+ffile) as f:
             for j in range(num):
                 rt = f.next()
                 rt = findall(r'time=(.*?) ms',rt)[0]
@@ -42,8 +44,8 @@ def getrtt(ffile,num):
     rtt = sorted(rtt)
     return rtt
 
-def graph1(exp,div,num):
-    rtts= getrtts('1/'+exp,num)
+def graph1(exp,crsid,div,num):
+    rtts= getrtts('1/'+exp,crsid,num)
     avgs = []
     for i in range(num):
         avg = np.median([rtts[j][i] for j in range(10)])
@@ -69,13 +71,13 @@ def graph1(exp,div,num):
     plt.errorbar(errxs, errys, yerr=minsmaxs,linestyle="none")
     plt.show()
 
-def graph5_000001(exp,usecs,num):
+def graph5_000001(exp,crsid,usecs,num):
     maxs_000001 = [[] for u in usecs]
     mins_000001 = [[] for u in usecs]
     meds_000001 = [[] for u in usecs]
     for u in range(len(usecs)):
         for i in range(10):
-            rtt = getrtt('5/'+exp+'_0.000001_'+str(usecs[u])+'_'+str(i), num)
+            rtt = getrtt('5/'+exp+'_0.000001_'+str(usecs[u])+'_'+str(i), crsid,num)
             maxs_000001[u].append(max(rtt))
             mins_000001[u].append(min(rtt))
             meds_000001[u].append(np.median(rtt))
@@ -113,13 +115,13 @@ def graph5_000001(exp,usecs,num):
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
 
-def graph5_001(exp,usecs,num):
+def graph5_001(exp,crsid,usecs,num):
     maxs_001 = [[] for u in usecs]
     mins_001 = [[] for u in usecs]
     meds_001 = [[] for u in usecs]
     for u in range(len(usecs)):
         for i in range(10):
-            rtt = getrtt('5/'+exp+'_0.001_'+str(usecs[u])+'_'+str(i), num)
+            rtt = getrtt('5/'+exp+'_0.001_'+str(usecs[u])+'_'+str(i), crsid,num)
             maxs_001[u].append(max(rtt))
             mins_001[u].append(min(rtt))
             meds_001[u].append(np.median(rtt))
@@ -157,10 +159,10 @@ def graph5_001(exp,usecs,num):
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
 
-def data_iperf(fexp):
+def data_iperf(fexp,crsid):
     bws = [[] for i in range(10)]
     for j in range(5):
-        with open('/root/L50Lab1/'+fexp+'_' + str(j)) as f:
+        with open('/root/'+crsid+'/L50Lab1/'+fexp+'_' + str(j)) as f:
             for i in range(10):
                 bw = f.next()
                 bww = findall(r'Bytes(.*?)Gbits/sec', bw)[0]
@@ -168,11 +170,11 @@ def data_iperf(fexp):
     return bws
 
 
-def data10():
+def data10(crsid):
     sbw = [[] for i in range(10)]
     cbw = [[] for i in range(10)]
     for j in range(5):
-        with open('/root/L50Lab1/10/exp10_' + str(j)) as f:
+        with open('/root/'+crsid+'/L50Lab1/10/exp10_' + str(j)) as f:
             f.next()
             f.next()
             for i in range(10):
@@ -184,12 +186,12 @@ def data10():
                 cbw[i].append(float(cbww))
     return sbw, cbw
 
-def data11(windows):
+def data11(windows,crsid):
     winds = []
     bws = [[] for w in windows]
     for b in range(len(windows)):
         for i in range(5):
-            with open('/root/L50Lab1/11/exp11_'+str(windows[b])+'_'+str(i)) as f:
+            with open('/root/'+crsid+'/L50Lab1/11/exp11_'+str(windows[b])+'_'+str(i)) as f:
                 wind = f.next()
                 if (i==0):
                     w = findall(r'size:(.*?)KByte', wind)[0]
@@ -200,12 +202,12 @@ def data11(windows):
     return winds, bws
 
 
-def data_band(fexp,bands):
+def data_band(fexp,crsid,bands):
     pcs = [[] for b in bands]
     bans = [[] for b in bands]
     for b in range(len(bands)):
         for i in range(5):
-            with open('/root/L50Lab1/'+fexp+'_'+str(bands[b])+'_'+str(i)) as f:
+            with open('/root/'+crsid+'/L50Lab1/'+fexp+'_'+str(bands[b])+'_'+str(i)) as f:
                 out = f.read()
                 ba = float(findall(r'Bytes(.*?)Mbits/sec',out)[0])
                 bans[b].append(ba)
@@ -214,11 +216,11 @@ def data_band(fexp,bands):
     bans = map(np.average,bans)
     return bans,pcs
 
-def data13b(windows):
+def data13b(windows,crsid):
     bws = [[] for w in windows]
     for b in range(len(windows)):
         for i in range(5):
-            with open('/root/L50Lab1/13/exp13b_'+str(windows[b])+'_'+str(i)) as f:
+            with open('/root/'+crsid+'/L50Lab1/13/exp13b_'+str(windows[b])+'_'+str(i)) as f:
                 for i in range(3):
                     f.next()
                 bb = f.next()
--- setup/mkdir1.sh
+++ setup/mkdir1.sh
@@ -1,4 +1,5 @@
-cd /root
+export CRSID=$1
+cd /root/$CRSID/
 mkdir L50Lab1
 cd L50Lab1
 mkdir 1  10  11  12  13  2  5  9
